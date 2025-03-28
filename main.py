13ef4bdb8e42f34447562c05f6427857e32adbd2
AtharvaZ <113021204+AtharvaZ@users.noreply.github.com>
2025-03-27 22:11:15 -0700

    add: tried and added API

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+api_key = "Z2WNVBjsRTheG0X19QRhwYZvuofO9bEtODB9IWXn"
+
+params = {
+    "api_key": api_key, 
+    "query": "orange",
+    "pageSize": 1
+}
+
+base_url = f"https://api.nal.usda.gov/fdc/v1/foods/search?"
+
+
+response = requests.get(url=base_url, params=params)
+
+data = response.json()
